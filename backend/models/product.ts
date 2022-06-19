ffb213a133a290b49a297facac70201a1f07858f
ShahabBngsh <shahabbng1999@gmail.com>
2022-06-19 10:44:05 +0500

    moved category, discount code to product.ts, it is only required in product

--- backend/models/product.ts
+++ backend/models/product.ts
@@ -1,13 +1,69 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ReviewSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+},
+{
+  timestamp: true
+});
+
+const DiscountSchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    desc: {
+      type: String
+    },
+    percentage: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 99
+    },
+    endsInDays: {
+      type: Number,
+      required: true,
+      min: 1
+    }
+  },
+  {
+    timestamp: true
+  });
+
 const ProductSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
   name: {
     type: String,
     required: true
   },
   desc: {
     type: String,
+  },
+  img: {
+    type: String,
+    required: true
   },
   price: {
     type: Number,
@@ -18,29 +74,27 @@ const ProductSchema = new Schema({
     default: 1,
     min: 0
   },
+  reviews: [ReviewSchema],
   rating: {
     type: Number,
     default: 0,
     min: 0,
     max: 5
   },
-  orderedBy: [{
-    type: String,
-  }],
+  numReviews: {
+    type: Number,
+    default: 0
+  },
   category: {
-    type: String
+    type: String,
+    required: true
   },
   discount: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now()
-  },
-  modifiedAt: {
-    type: Date,
-    default: Date.now()
+    type: DiscountSchema
   }
+},
+{
+  timestamp: true
 })
 
-module.exports = mongoose.model('product', ProductSchema);
+module.exports = mongoose.model('Product', ProductSchema);
