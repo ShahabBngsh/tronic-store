5bd43128a988e8bf82099f4bafc57aa94695cfef
ShahabBngsh <shahabbng1999@gmail.com>
2022-07-19 12:12:26 +0500

    removed quantity field from the product model

--- backend/models/product.js
+++ backend/models/product.js
@@ -73,12 +73,6 @@ const ProductSchema = new Schema({
     type: Number,
     required: [true, 'Please enter the price for this product']
   },
-  quantity: {
-    type: Number,
-    default: 1,
-    min: 0
-  },
-  reviews: [ReviewSchema],
   rating: {
     type: Number,
     default: 0,
@@ -93,9 +87,19 @@ const ProductSchema = new Schema({
     type: String,
     required: true
   },
+  initialStock: {
+    type: Number,
+    required: true
+  },
+  currentStock: {
+    type: Number,
+    default: 0,
+    required: true
+  },
   discount: {
     type: DiscountSchema
-  }
+  },
+  reviews: [ReviewSchema]
 },
   {
     timestamp: true
